26a4a84faf85e2e6ceaa8a2ec2ecddb772619629
Bucha1958 <okoriemmadu@gmail.com>
2023-05-12 08:25:41 +0000

    OOP

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-class square():
+class Square():
     """Square class created"""    
     width = 0
     height = 0
@@ -11,21 +11,21 @@ class square():
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area_of_my_square(self):
+    def area(self):
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def perimeter(self):
         """ Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """configure the format for print this class. """
-        return "{}/{}".format(self.width, self.height)
+        """Configure the format for print this class. """
+        return "Square(width={}, height={})".format(self.width, self.height)
 
 if __name__ == "__main__":
 
     s = square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print("Area:", s.area())
+    print("Perimeter:", s.perimeter())
