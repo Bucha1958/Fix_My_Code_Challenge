0746e9e8afa15a06277285a165c6001e633a6901
Bucha1958 <okoriemmadu@gmail.com>
2023-05-12 13:16:05 +0000

    Correction

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,45 @@
 #!/usr/bin/python3
+"""
+    A script that will create a class square
+"""
 
 
-class square():
+class Square():
+    """Square class created"""
 
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        if args:
+            try:
+                self.width, self.height = args
+            except (ValueError, TypeError):
+                raise ValueError
+        else:
+            try:
+                self.width = kwargs.get("width")
+                self.height = kwargs.get("height")
+            except (ValueError, TypeError):
+                raise ValueError
+        if self.width != self.height:
+            raise ValueError
 
     def area_of_my_square(self):
-        """Area of the square"""
-        return self.width * self.height
+        """ Area of the square """
+        return self.width * self.width
 
-    def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height *2)
+    def perimeter_of_my_square(self):
+        """ Perimeter of the square"""
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        """Configure the format for print this class. """
+        return "Square(width={}, height={})".format(self.width, self.height)
 
-    if __name__ == "__main__":
 
-        s = square(width=12, height=9)
-        print(s)
-        print(s.area_of_my_square())
-        print(s.PermiterOfMySquare())
+if __name__ == "__main__":
+    s = Square(width=12, height=12)
+    print(s)
+    print("Area:", s.area_of_my_square())
+    print("Perimeter:", s.perimeter_of_my_square())
